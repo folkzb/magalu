77ceb9dc38110c206ac365a95b0589366a750063
Pedro Soares <hspedro@profusion.mobi>
2023-07-25 17:49:00 -0300

    feat(sdk): openapi Execute to send HTTP request

--- mgc/sdk/openapi/operation.go
+++ mgc/sdk/openapi/operation.go
@@ -3,6 +3,10 @@ package openapi
 import (
 	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
 	"regexp"
 	"strings"
 
@@ -177,24 +181,155 @@ func (o *Operation) ConfigsSchema() *core.Schema {
 	return o.configsSchema
 }
 
-func getServerURL(servers openapi3.Servers, path string, configs map[string]core.Value) (string, error) {
+func (o *Operation) getServerURL(configs map[string]core.Value) (string, error) {
+	// TODO: implement configs map[string]core.Value to replace variables in serverUrl
 	var s *openapi3.Server
-	if len(servers) > 0 {
-		s = servers[0]
+	if len(o.servers) > 0 {
+		s = o.servers[0]
 	}
 
 	if s == nil {
 		return "", fmt.Errorf("no available servers in spec")
 	}
-
-	// TODO: Map Server Variables to URL based on received config
-	return s.URL + path, nil
+	return s.URL + o.key, nil
 }
 
-func (o *Operation) Execute(ctx context.Context, parameters map[string]core.Value, configs map[string]core.Value) (result core.Value, err error) {
+func replaceInPath(path string, param *openapi3.Parameter, val core.Value) (string, error) {
+	var strValue string
+	// TODO: handle complex conversion using openapi style values
+	// https://spec.openapis.org/oas/latest.html#style-values
+	switch valType := val.(type) {
+	case []core.Value:
+		return "", fmt.Errorf("Can not replace a slice into URL path: %v, param: %v", valType, param.Name)
+	case map[core.Value]core.Value:
+		return "", fmt.Errorf("Can not replace a map into URL path: %v, param: %v", valType, param.Name)
+	default:
+		strValue = fmt.Sprintf("%v", val)
+	}
+	paramTemplate := "{" + param.Name + "}"
+	return strings.ReplaceAll(path, paramTemplate, strValue), nil
+}
+
+func addQueryParam(qValues *url.Values, param *openapi3.Parameter, value core.Value) {
+	// TODO: handle complex conversion using openapi style values
+	// https://spec.openapis.org/oas/latest.html#style-values
+	if value == nil || fmt.Sprintf("%v", value) == "" {
+		return
+	}
+	qValues.Set(param.Name, fmt.Sprintf("%v", value))
+}
+
+func addHeaderParam(req *http.Request, param *openapi3.Parameter, val core.Value) {
+	// TODO: handle complex types passed on val
+	req.Header.Set(param.Name, fmt.Sprintf("%v", val))
+}
+
+func addCookieParam(req *http.Request, param *openapi3.Parameter, val core.Value) {
+	// TODO: handle complex types passed on val
+	req.AddCookie(&http.Cookie{
+		Name:  param.Name,
+		Value: fmt.Sprintf("%v", val),
+	})
+}
+
+func (o *Operation) buildRequestFromParams(
+	paramValues map[string]core.Value,
+	configs map[string]core.Value,
+) (*http.Request, error) {
+	serverURL, err := o.getServerURL(configs)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(strings.ToUpper(o.method), serverURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	queryValues := url.Values{}
+	for _, ref := range o.operation.Parameters {
+		parameter := ref.Value
+		name := getNameExtension(o.extensionPrefix, parameter.Extensions, parameter.Name)
+
+		switch parameter.In {
+		case openapi3.ParameterInPath:
+			serverURL, err = replaceInPath(serverURL, parameter, paramValues[name])
+			if err != nil {
+				return nil, err
+			}
+		case openapi3.ParameterInQuery:
+			addQueryParam(&queryValues, parameter, paramValues[name])
+		case openapi3.ParameterInHeader:
+			addHeaderParam(req, parameter, configs[name])
+		case openapi3.ParameterInCookie:
+			addCookieParam(req, parameter, configs[name])
+		default:
+			fmt.Printf("Unrecognizable param %s location %s", parameter.Name, parameter.In)
+		}
+	}
+	if len(queryValues) > 0 {
+		req.URL, err = url.Parse(serverURL + "?" + queryValues.Encode())
+	} else {
+		req.URL, err = url.Parse(serverURL)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func setSecurityHeader(req *http.Request, secRequirements *openapi3.SecurityRequirements) error {
+	if secRequirements == nil {
+		return nil
+	}
+	for _, reqRef := range *secRequirements {
+		for secScheme := range reqRef {
+			if "oauth2" == strings.ToLower(secScheme) {
+				// TODO: get token from config
+				access_token := os.Getenv("MGC_SDK_ACCESS_TOKEN")
+				if access_token == "" {
+					return fmt.Errorf("Could not read acess token from env MGC_SDK_ACCESS_TOKEN")
+				}
+				req.Header.Set("Authorization", "Bearer "+access_token)
+				return nil
+			}
+		}
+	}
+	return nil
+}
+
+// TODO: refactor this closer to the client that comes from a context
+func (o *Operation) createHttpRequest(paramValues map[string]core.Value, configs map[string]core.Value) (*http.Request, error) {
+	req, err := o.buildRequestFromParams(paramValues, configs)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: accept everything, but later we need to fine-grain if json, multipart, etc
+	req.Header.Add("Accept", "*/*")
+	req.Header.Add("Accept-Encoding", "gzip, deflate, br")
+	req.Header.Add("Connection", "keep-alive")
+
+	if err := setSecurityHeader(req, o.operation.Security); err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+func (o *Operation) Execute(
+	ctx context.Context,
+	parameters map[string]core.Value,
+	configs map[string]core.Value,
+) (result core.Value, err error) {
 	// load definitions if not done yet
 	parametersSchema := o.ParametersSchema()
 	configsSchema := o.ConfigsSchema()
+
+	client := core.HttpClientFromContext(ctx)
+	if client == nil {
+		return nil, fmt.Errorf("No HTTP client configured")
+	}
 
 	if err = parametersSchema.VisitJSON(parameters); err != nil {
 		return nil, err
@@ -204,12 +339,30 @@ func (o *Operation) Execute(ctx context.Context, parameters map[string]core.Valu
 		return nil, err
 	}
 
-	serverURL, _ := getServerURL(o.servers, o.key, configs)
+	req, err := o.createHttpRequest(parameters, configs)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Error on request response body: %s", err)
+	}
 
-	fmt.Println("server: " + serverURL)
-	fmt.Printf("TODO: execute: %v %v\ninput: p=%v; c=%v\ndefinitions: p=%v; c=%v\n", o.method, o.key, parameters, configs, parametersSchema.Properties, configsSchema)
+	var data core.Value
+	switch contentType := core.GetContentType(resp); contentType {
+	default:
+		// TODO: Handle other content types
+		log.Fatalf("Unrecognized content-type %s in the response. Aborting", contentType)
+	case "":
+		// This will happen for 204 - No Content returns with empty body
+		return map[string]core.Value{}, err
+	case "application/json":
+		data = map[string]core.Value{}
+		err := core.DecodeJSON(resp, &data)
+		return data, err
+	}
 
-	return nil, fmt.Errorf("not implemented")
+	return data, err
 }
 
 var _ core.Executor = (*Operation)(nil)
