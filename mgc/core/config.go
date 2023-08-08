0b0f598d0870cd4c95854f6c9e519ecbd39c66c4
Pedro Mota <pedro.mota@profusion.mobi>
2023-08-08 17:13:52 -0300

    fix(config): check if config file exists before config operations
    If there is no config file set, operations like Delete and Set
    should create the config file instead of returning an error
    
    Issue #144

--- mgc/core/config.go
+++ mgc/core/config.go
@@ -2,22 +2,29 @@ package core
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"log"
 	"os"
+	"path"
 
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"gopkg.in/yaml.v3"
 )
 
-type Config struct{}
+type Config struct {
+	path     string
+	fileName string
+}
 
 const (
-	CONFIG_NAME = "config"
-	CONFIG_TYPE = "yaml"
-	CONFIG_PATH = "$HOME/.mgc"
+	CONFIG_NAME     = "config"
+	CONFIG_TYPE     = "yaml"
+	CONFIG_FOLDER   = ".mgc"
+	CONFIG_FILE     = CONFIG_NAME + "." + CONFIG_TYPE
+	FILE_PERMISSION = 0777 // TODO: investigate how to lower permission
 )
-
-const FILE_PERMISSION = 0644
 
 var configKey contextKey = "magalu.cloud/core/Config"
 
@@ -31,15 +38,20 @@ func ConfigFromContext(ctx context.Context) *Config {
 }
 
 func NewConfig() *Config {
-	viper.SetConfigName(CONFIG_NAME)
-	viper.SetConfigType(CONFIG_TYPE)
-	viper.AddConfigPath(CONFIG_PATH)
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err != nil {
+	path, err := configFilePath()
+	if err != nil {
+		// TODO: when it's done, use logger instead
+		log.Println(err)
 		return &Config{}
 	}
-	return &Config{}
+
+	viper.SetConfigName(CONFIG_NAME)
+	viper.SetConfigType(CONFIG_TYPE)
+	viper.AddConfigPath(path)
+	viper.AutomaticEnv()
+
+	_ = viper.ReadInConfig()
+	return &Config{path: path, fileName: CONFIG_FILE}
 }
 
 func (c *Config) BindPFlag(key string, flag *pflag.Flag) error {
@@ -55,11 +67,13 @@ func (c *Config) Get(key string) any {
 }
 
 func (c *Config) Set(key string, value interface{}) error {
-	configMap := viper.AllSettings()
-	configMap[key] = value
+	if err := os.MkdirAll(c.path, FILE_PERMISSION); err != nil && !errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("error creating dir at %s: %w", c.path, err)
+	}
+	viper.Set(key, value)
 
-	if err := saveToConfigFile(configMap); err != nil {
-		return err
+	if err := viper.WriteConfigAs(path.Join(c.path, c.fileName)); err != nil && !errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("error writing to config file: %w", err)
 	}
 
 	return nil
@@ -69,22 +83,25 @@ func (c *Config) Delete(key string) error {
 	configMap := viper.AllSettings()
 	delete(configMap, key)
 
-	if err := saveToConfigFile(configMap); err != nil {
+	if err := saveToConfigFile(c, configMap); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func saveToConfigFile(configMap map[string]interface{}) error {
+func saveToConfigFile(c *Config, configMap map[string]interface{}) error {
 	encodedConfig, err := yaml.Marshal(configMap)
 	if err != nil {
 		return err
 	}
 
-	f := viper.ConfigFileUsed()
-	if err = os.WriteFile(f, encodedConfig, FILE_PERMISSION); err != nil {
-		return err
+	if err = os.MkdirAll(c.path, FILE_PERMISSION); err != nil && !errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("error creating dir at %s: %w", c.path, err)
+	}
+
+	if err = os.WriteFile(path.Join(c.path, c.fileName), encodedConfig, FILE_PERMISSION); err != nil {
+		return fmt.Errorf("error writing to config file: %w", err)
 	}
 
 	if err := viper.ReadInConfig(); err != nil {
@@ -92,4 +109,17 @@ func saveToConfigFile(configMap map[string]interface{}) error {
 	}
 
 	return nil
+}
+
+func configFilePath() (string, error) {
+	dir, err := os.UserHomeDir()
+	if err == nil {
+		return path.Join(dir, CONFIG_FOLDER), nil
+	}
+	dir, wdErr := os.Getwd()
+	if dir == "" || wdErr != nil {
+		return "", errors.Join(fmt.Errorf("Error: unable to access home and current working directory"), wdErr, err)
+	}
+
+	return path.Join(dir, CONFIG_FOLDER), nil
 }
