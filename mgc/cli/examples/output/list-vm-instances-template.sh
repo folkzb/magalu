d6bc7c39e6a8824163b1f46f18e4c43e149fd51c
Leonardo Bronstein Franceschetti <leonardo.franceschetti@profusion.mobi>
2023-08-10 16:50:04 -0300

    feat: add example script for template output formatters
    [#141]

---
+++ mgc/cli/examples/output/list-vm-instances-template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -xe
+
+# Color trace output for commands to stand out
+PS4='\[\e[36m\]\+ \[\e[m\]'
+
+MGC_CLI=${MGC_CLI:-./mgc}
+D=`dirname $0`
+
+# 1. Login
+$MGC_CLI auth login
+
+# 2. List instances with inline Go string template output
+STR_TEMPLATE="{{range .instances}}Name: {{.name}}, ID: {{.id}}{{printf \"%s\" \"\n\"}}{{end}}"
+$MGC_CLI virtual-machine instances list -o "template=$STR_TEMPLATE"
+
+# 3. List instances with pre-defined Go string template via file
+STR_TEMPLATE_FILE="$D/list-vm-instances-template.txt"
+$MGC_CLI virtual-machine instances list -o "template-file=$STR_TEMPLATE_FILE"
