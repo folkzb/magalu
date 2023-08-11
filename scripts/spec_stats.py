8fc60a6a764d44547c0f93f619c44c6408caca7c
Leonardo Bronstein Franceschetti <leonardo.franceschetti@profusion.mobi>
2023-08-11 18:24:22 -0300

    feat(stat-script): organize operations in resources, like SDK does

--- scripts/spec_stats.py
+++ scripts/spec_stats.py
@@ -13,22 +13,24 @@ class OAPI(NamedTuple):
     schema: OAPISchema
 
 
-class ResponseContext(NamedTuple):
+class OAPIOperation(NamedTuple):
     path: str
     method: str
-    code: str
-
-
-class OperationContext(NamedTuple):
-    path: str
-    method: str
+    fields: OAPISchema
 
     def key(self) -> str:
         return self.method.upper() + " " + self.path
 
 
-class PathContext(NamedTuple):
+class OAPIResource(NamedTuple):
     name: str
+    operations: List[OAPIOperation]
+
+
+class ResponseContext(NamedTuple):
+    path: str
+    method: str
+    code: str
 
 
 # This is used to fix list indentations, as Pyyaml doesn't indent them :/
@@ -76,8 +78,8 @@ def load_oapis(d: str) -> [OAPI]:
     return schemas
 
 
-def fill_responses_stats(ctx: OperationContext, responses: OAPISchema, dst: OAPIStats):
-    obj = {ctx.key(): []}
+def fill_responses_stats(op: OAPIOperation, responses: OAPISchema, dst: OAPIStats):
+    obj = {op.key(): []}
 
     for code, r in responses.items():
         content = r.get("content", {})
@@ -87,49 +89,65 @@ def fill_responses_stats(ctx: OperationContext, responses: OAPISchema, dst: OAPI
 
         for t, _ in content.items():
             if t != "application/json":
-                obj[ctx.key()].append({code: t})
+                obj[op.key()].append({code: t})
 
-    if obj[ctx.key()] and filterer.should_include("non-json-responses"):
+    if obj[op.key()] and filterer.should_include("non-json-responses"):
         dst.setdefault("non-json-responses", []).append(obj)
 
 
-def fill_req_body_stats(ctx: OperationContext, r: OAPISchema, dst: OAPIStats):
+def fill_req_body_stats(op: OAPIOperation, r: OAPISchema, dst: OAPIStats):
     content = r.get("content", {})
     if content:
         for t, _ in content.items():
             if t != "application/json" and filterer.should_include("non-json-requests"):
-                dst.setdefault("non-json-requests", []).append({ctx.key(): t})
+                dst.setdefault("non-json-requests", []).append({op.key(): t})
 
 
-def fill_operation_stats(ctx: OperationContext, o: OAPISchema, dst: OAPIStats):
-    responses = o.get("responses", {})
+def fill_operation_stats(op: OAPIOperation, dst: OAPIStats):
+    responses = op.fields.get("responses", {})
     if responses:
-        fill_responses_stats(ctx, responses, dst)
+        fill_responses_stats(op, responses, dst)
 
-    req_body = o.get("requestBody", {})
+    req_body = op.fields.get("requestBody", {})
     if req_body:
-        fill_req_body_stats(ctx, req_body, dst)
+        fill_req_body_stats(op, req_body, dst)
 
-    if "operationId" not in o and filterer.should_include("missing_operation_id"):
-        dst.setdefault("missing_operation_id", []).append(ctx.key())
+    if "operationId" not in op.fields and filterer.should_include(
+        "missing_operation_id"
+    ):
+        dst.setdefault("missing_operation_id", []).append(op.key())
 
     return
 
 
-def fill_path_stats(ctx: PathContext, p: OAPISchema, dst: OAPIStats):
-    for method, o in p.items():
-        if isinstance(o, dict) and method in OPERATION_KEYS:
-            op_ctx = OperationContext(path=ctx.name, method=method)
-            fill_operation_stats(op_ctx, o, dst)
-    return
+def fill_resource_stats(r: OAPIResource, dst: OAPIStats):
+    for op in r.operations:
+        fill_operation_stats(op, dst)
+
+
+def fill_resources(o: OAPI, dst: Dict[str, OAPIResource]):
+    for pn, p in o.schema.get("paths", {}).items():
+        for path_field, sub_fields in p.items():
+            if not isinstance(sub_fields, dict) or path_field not in OPERATION_KEYS:
+                continue
+
+            op = OAPIOperation(path=pn, method=path_field, fields=sub_fields)
+            tags = sub_fields.get("tags")
+
+            if tags:
+                res_name = tags[0]
+                dst.setdefault(
+                    res_name, OAPIResource(name=res_name, operations=[])
+                ).operations.append(op)
 
 
 def get_oapi_stats(o: OAPI) -> OAPIStats:
     result = {}
+    resources = {}
+    fill_resources(o, resources)
 
-    for pn, p in o.schema.get("paths", {}).items():
-        ctx = PathContext(name=pn)
-        fill_path_stats(ctx, p, result)
+    for res in resources.values():
+        fill_resource_stats(res, result)
 
     # TODO: Add stats for other fields
 
