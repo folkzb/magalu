03d7f5c20c989007c761ff81224037f576b0fa93
Gustavo Sverzut Barbieri <barbieri@profusion.mobi>
2023-09-28 20:19:06 -0300

    feat(core/utils): add more compare functions

--- mgc/core/utils/compare.go
+++ mgc/core/utils/compare.go
@@ -17,14 +17,40 @@ func IsSameValueOrPointer(a, b any) bool {
 		return false
 	}
 
-	if vA.Kind() == reflect.Pointer {
-		vA = vA.Elem()
-		vB = vB.Elem()
-	}
+	vA = valueDereference(vA)
+	vB = valueDereference(vB)
 
 	if vA.Comparable() {
 		return vA.Interface() == vB.Interface()
 	} else {
 		return vA.UnsafePointer() == vB.UnsafePointer()
 	}
+}
+
+func IsComparableEqual[V comparable](a, b V) bool {
+	return a == b
+}
+
+func IsComparablePointerEqual[V comparable](a, b *V) bool {
+	return IsPointerEqualFunc[V](a, b, func(v1, v2 *V) bool {
+		return IsComparableEqual[V](*a, *b)
+	})
+}
+
+// equals is only called if both pointers are non-nil
+func IsPointerEqualFunc[V any](a, b *V, equals func(*V, *V) bool) bool {
+	if a == b {
+		return true
+	}
+	if a == nil || b == nil {
+		return false
+	}
+	return equals(a, b)
+}
+
+func valueDereference(value reflect.Value) reflect.Value {
+	for value.Kind() == reflect.Pointer && !value.IsNil() {
+		value = value.Elem()
+	}
+	return value
 }
