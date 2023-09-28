a87a555857089bf555d6e03ea1bbba500b776c3b
Victor Accarini <victor.accarini@profusion.mobi>
2023-09-28 17:15:20 -0300

    feat(scripts): remove x-tenant-id param when used as oapi ref

--- scripts/remove_tenant_id.py
+++ scripts/remove_tenant_id.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, List, Tuple
 import argparse
 import yaml
 
@@ -16,24 +16,70 @@ def save_external(spec: OAPISchema, path: str):
         yaml.dump(spec, fd, sort_keys=False, indent=4, allow_unicode=True)
 
 
+def filter_params_and_refs(
+    params: List[str], spec: OAPISchema, param_name: str
+) -> Tuple[List[str], List[str]]:
+    refs = []
+    filtered_params = []
+    for p in params:
+        pv = p
+        ref = None
+
+        if "$ref" in pv:
+            ref = pv.get("$ref")
+            ref_path = ref.removeprefix("#/").split("/")
+
+            pv = spec
+            for rp in ref_path:
+                pv = pv[rp]
+
+        if pv.get("name") != param_name:
+            filtered_params.append(p)
+        elif ref is not None:
+            refs.append(ref)
+            ref = None
+
+    return filtered_params, refs
+
+
+def remove_param_refs(spec: OAPISchema, refs: List[str]):
+    def should_delete(value: Any, keys: List[str]):
+        if len(keys) == 0:
+            return True
+        else:
+            if should_delete(value[keys[0]], keys[1:]):
+                del value[keys[0]]
+
+            return len(value) == 0
+
+    for ref in refs:
+        ref_path = ref.removeprefix("#/").split("/")
+        if should_delete(spec, ref_path):
+            del spec[ref_path[0]]
+
+
 def remove_param(spec: OAPISchema, param_name: str):
+    refs_for_removal = set()
     for path in spec.get("paths", {}).values():
         for action in path.values():
             if not isinstance(action, dict) or "parameters" not in action:
                 continue
 
-            filtered_params = [
-                p for p in action.get("parameters", [{}]) if p.get("name") != param_name
-            ]
+            filtered_params, removable_refs = filter_params_and_refs(
+                action.get("parameters", [{}]), spec, param_name
+            )
+            refs_for_removal.update(removable_refs)
 
             if not filtered_params:
                 del action["parameters"]
             else:
                 action["parameters"] = filtered_params
 
+    remove_param_refs(spec, refs_for_removal)
+
 
 def remove_tenant_id(spec: OAPISchema):
-    return remove_param(spec, param_name="x-tenant-id")
+    remove_param(spec, param_name="x-tenant-id")
 
 
 if __name__ == "__main__":
