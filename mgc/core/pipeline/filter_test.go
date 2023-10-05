d9f20adb99a81fb612d9297802663f86c60e27ad
Rafael Oliveira <rafael.oliveira@profusion.mobi>
2023-10-05 11:08:50 -0300

    test(pipeline): add filter test

---
+++ mgc/core/pipeline/filter_test.go
@@ -0,0 +1,79 @@
+package pipeline_test
+
+import (
+	"context"
+	"testing"
+
+	"golang.org/x/exp/constraints"
+	"magalu.cloud/core/pipeline"
+)
+
+type EvenFilter[T constraints.Integer] struct{}
+
+func (f EvenFilter[T]) Filter(ctx context.Context, entry T) pipeline.FilterStatus {
+	if int64(entry)%2 == 0 {
+		return pipeline.FilterInclude
+	} else {
+		return pipeline.FilterExclude
+	}
+}
+
+type ThreeFilter[T constraints.Integer] struct{}
+
+func (f ThreeFilter[T]) Filter(ctx context.Context, entry T) pipeline.FilterStatus {
+	if int64(entry)%3 == 0 {
+		return pipeline.FilterInclude
+	} else {
+		return pipeline.FilterExclude
+	}
+}
+
+var OddFilterInstance = pipeline.FilterRuleNot[int]{Not: EvenFilter[int]{}}
+
+func TestFilter(t *testing.T) {
+	ctx := context.Background()
+	genChan := pipeline.RangeGenerator(10)
+
+	filteredChan := pipeline.Filter[int](ctx, genChan, EvenFilter[int]{})
+
+	for num := range filteredChan {
+		if num%2 != 0 {
+			t.Error("Found odd number after EvenFilter: ", num)
+		}
+	}
+}
+
+func TestNotFilter(t *testing.T) {
+	ctx := context.Background()
+	genChan := pipeline.RangeGenerator(10)
+
+	filteredChan := pipeline.Filter[int](ctx, genChan, OddFilterInstance)
+
+	for num := range filteredChan {
+		if num%2 == 0 {
+			t.Error("Found even number after OddFilter: ", num)
+		}
+	}
+}
+
+func TestAllFilter(t *testing.T) {
+	// TODO this test has been started but is not working properly
+	t.Skip()
+	ctx := context.Background()
+	genChan := pipeline.RangeGenerator(15)
+
+	filter := pipeline.FilterRuleAll[int]{
+		All: []pipeline.FilterRule[int]{
+			EvenFilter[int]{},
+			ThreeFilter[int]{},
+		},
+	}
+
+	filteredChan := pipeline.Filter[int](ctx, genChan, filter)
+
+	for num := range filteredChan {
+		if num%2 != 0 || num%3 != 0 {
+			t.Error("Found non multiple of 6 after AllFilter: ", num)
+		}
+	}
+}
