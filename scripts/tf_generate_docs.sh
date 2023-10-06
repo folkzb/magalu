beed646bdce28ebf73d2ddcd95fd403eacf57d23
Pedro Mota <pedromota@Pedros-MacBook-Air.local>
2023-10-06 15:45:27 -0300

    fix(tf): set env var on doc script

--- scripts/tf_generate_docs.sh
+++ scripts/tf_generate_docs.sh
@@ -4,7 +4,8 @@ set -xe
 
 PROVIDER_DIR="./mgc/terraform-provider-mgc/"
 TF_PLUGIN_DOCS="github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs@v0.16.0"
+ABSOLUTE_PATH_OAPI=$(realpath ${MGC_SDK_OPENAPI_DIR:-./mgc/cli/openapis})
 
 mkdir -p $PROVIDER_DIR/docs
 
-go run $TF_PLUGIN_DOCS generate --provider-dir=$PROVIDER_DIR
+MGC_SDK_OPENAPI_DIR=$ABSOLUTE_PATH_OAPI go run $TF_PLUGIN_DOCS generate --provider-dir=$PROVIDER_DIR
