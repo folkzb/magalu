ec9073d532e97e3474561dd4ce435f69b02acfba
Rafael Oliveira <rafael.oliveira@profusion.mobi>
2023-10-09 16:35:02 -0300

    feat(pipeline): add logger

---
+++ mgc/core/pipeline/logger.go
@@ -0,0 +1,41 @@
+package pipeline
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+)
+
+var pkgLogger *zap.SugaredLogger
+
+func logger() *zap.SugaredLogger {
+	if pkgLogger == nil {
+		pkgLogger = zap.Must(zap.NewProduction()).Sugar()
+	}
+	return pkgLogger
+}
+
+type contextLoggerKey string
+
+var ctxWrappedKey contextLoggerKey = "magalu.cloud/core/pipeline"
+
+// Get the logger from context or return the default logger.
+//
+// Loggers are used from context in order to avoid passing it explicitly everywhere,
+// which would make APIs cumbersome to use.
+func FromContext(ctx context.Context) *zap.SugaredLogger {
+	if v := ctx.Value(ctxWrappedKey); v != nil {
+		if l, ok := v.(*zap.SugaredLogger); ok {
+			return l
+		}
+	}
+	return logger()
+}
+
+// Create a new context with a new logger.
+//
+// Any existing loggers will be superseded by the given one in the returned context and others.
+// Parent context logger (if exists) is untouched.
+func NewContext(parentCtx context.Context, logger *zap.SugaredLogger) context.Context {
+	return context.WithValue(parentCtx, ctxWrappedKey, logger)
+}
