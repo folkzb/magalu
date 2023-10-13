b63770fadc715ef4b0e878375291706dbe7ce0c6
Gustavo Sverzut Barbieri <barbieri@profusion.mobi>
2023-10-13 12:48:39 -0300

    refactor(core): MergeGroup now uses GrouperLazyChildren
    Reduce repeated code

--- mgc/core/merge.go
+++ mgc/core/merge.go
@@ -1,18 +1,16 @@
 package core
 
-import "fmt"
-
 type MergeGroup struct {
 	name        string
 	version     string
 	description string
-	merge       []Grouper
-	children    []Descriptor
-	childByName map[string]Descriptor
+	*GrouperLazyChildren[Descriptor]
 }
 
 func NewMergeGroup(name string, version string, description string, merge []Grouper) *MergeGroup {
-	return &MergeGroup{name, version, description, merge, nil, nil}
+	return &MergeGroup{name, version, description, NewGrouperLazyChildren[Descriptor](func() ([]Descriptor, error) {
+		return createChildren(merge)
+	})}
 }
 
 // BEGIN: Descriptor interface:
@@ -33,14 +31,14 @@ func (o *MergeGroup) Description() string {
 
 // BEGIN: Grouper interface:
 
-func (o *MergeGroup) mergeAfter(target Grouper, start int) []Grouper {
-	result := make([]Grouper, 1, len(o.merge)-start+1)
+func mergeAfter(toMerge []Grouper, target Grouper, start int) (result []Grouper) {
+	result = make([]Grouper, 1, len(toMerge)-start+1)
 	result[0] = target
 
 	name := target.Name()
 
-	for i := start; i < len(o.merge); i += 1 {
-		child, err := o.merge[i].GetChildByName(name)
+	for i := start; i < len(toMerge); i += 1 {
+		child, err := toMerge[i].GetChildByName(name)
 		if err == nil {
 			if group, ok := child.(Grouper); ok {
 				result = append(result, group)
@@ -50,81 +48,47 @@ func (o *MergeGroup) mergeAfter(target Grouper, start int) []Grouper {
 	return result
 }
 
-func (o *MergeGroup) getChildren() (children []Descriptor, byName map[string]Descriptor, err error) {
-	if o.children != nil {
-		return o.children, o.childByName, nil
-	}
+func createChildren(toMerge []Grouper) (children []Descriptor, err error) {
+	children = make([]Descriptor, 0)
+	used := make(map[string]bool)
 
-	o.children = make([]Descriptor, 0)
-	o.childByName = make(map[string]Descriptor)
-
-	for i, c := range o.merge {
+	for i, c := range toMerge {
 		finished, err := c.VisitChildren(func(child Descriptor) (run bool, err error) {
 			name := child.Name()
 
-			if o.childByName[name] != nil {
+			if used[name] {
 				return true, nil
 			}
 
 			if group, ok := child.(Grouper); ok {
-				merge := o.mergeAfter(group, i+1)
-				if len(merge) > 1 {
-					child = &MergeGroup{
-						name:        name,
-						version:     group.Version(),
-						description: group.Description(),
-						merge:       merge,
-					}
+				merged := mergeAfter(toMerge, group, i+1)
+				if len(merged) > 1 {
+					child = NewMergeGroup(
+						name,
+						group.Version(),
+						group.Description(),
+						merged,
+					)
 				}
 			}
 
-			o.children = append(o.children, child)
-			o.childByName[name] = child
+			children = append(children, child)
+			used[name] = true
 			return true, nil
 		})
 
 		if err != nil {
-			return nil, nil, err
+			return nil, err
 		}
 		if !finished {
-			return nil, nil, err
+			return nil, err
 		}
 	}
 
-	return o.children, o.childByName, nil
+	return children, nil
 }
 
-func (o *MergeGroup) VisitChildren(visitor DescriptorVisitor) (finished bool, err error) {
-	children, _, err := o.getChildren()
-	if err != nil {
-		return false, err
-	}
-
-	for _, res := range children {
-		run, err := visitor(res)
-		if err != nil {
-			return false, err
-		}
-		if !run {
-			return false, nil
-		}
-	}
-	return true, nil
-}
-
-func (o *MergeGroup) GetChildByName(name string) (child Descriptor, err error) {
-	_, byName, err := o.getChildren()
-	if err != nil {
-		return nil, err
-	}
-	res, ok := byName[name]
-	if !ok {
-		return nil, fmt.Errorf("child not found: %s", name)
-	}
-
-	return res, nil
-}
-
+// implemented by embedded GrouperLazyChildren
 var _ Grouper = (*MergeGroup)(nil)
 
 // END: Grouper interface
