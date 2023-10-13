c696077ae2f67045416f5fa4eea27fbb08728255
Gustavo Sverzut Barbieri <barbieri@profusion.mobi>
2023-10-13 12:33:48 -0300

    feat(core): add GrouperLazyChildren
    We're seeing a pattern of groupers that will lazy-load children and
    keeping an array for ordered walking with VisitChildren() and a map
    byName for GetChildByName().
    
    Move this to its own struct and reuse it embedded in some places,
    reducing the repeated code.

---
+++ mgc/core/grouper_lazy_children.go
@@ -0,0 +1,81 @@
+package core
+
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/exp/slices"
+)
+
+// Implements VisitChildren() and GetChildByName() by calling createChildren()
+// only once, when/if needed, and storing the results.
+//
+// Children will be automatically sorted by their names, so visitor are always in a predictable and stable order.
+type GrouperLazyChildren[T Descriptor] struct {
+	createChildren func() ([]T, error)
+	children       []T
+	childByName    map[string]T
+}
+
+func NewGrouperLazyChildren[T Descriptor](createChildren func() ([]T, error)) *GrouperLazyChildren[T] {
+	if createChildren == nil {
+		panic("createChildren == nil")
+	}
+	return &GrouperLazyChildren[T]{createChildren, nil, nil}
+}
+
+func (g *GrouperLazyChildren[T]) getChildren() (children []T, byName map[string]T, err error) {
+	if g.createChildren == nil {
+		return g.children, g.childByName, nil
+	}
+
+	children, err = g.createChildren()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	g.createChildren = nil // avoid reloading and also free/GC any resources used by the loader function
+	g.children = children
+	g.childByName = make(map[string]T, len(children))
+
+	for _, child := range children {
+		g.childByName[child.Name()] = child
+	}
+
+	slices.SortFunc(g.children, func(a, b T) int {
+		return strings.Compare(a.Name(), b.Name())
+	})
+
+	return g.children, g.childByName, nil
+}
+
+func (g *GrouperLazyChildren[T]) VisitChildren(visitor DescriptorVisitor) (finished bool, err error) {
+	children, _, err := g.getChildren()
+	if err != nil {
+		return false, err
+	}
+
+	for _, child := range children {
+		run, err := visitor(child)
+		if err != nil {
+			return false, err
+		}
+		if !run {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+func (g *GrouperLazyChildren[T]) GetChildByName(name string) (child Descriptor, err error) {
+	_, childByName, err := g.getChildren()
+	if err != nil {
+		return nil, err
+	}
+	child, ok := childByName[name]
+	if !ok {
+		return nil, fmt.Errorf("child not found: %s", name)
+	}
+
+	return child, nil
+}
