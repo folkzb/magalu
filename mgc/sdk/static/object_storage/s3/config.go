9bd85be9d8d00b5a05d66cdebe5af2c6c6ef454b
Rafael Oliveira <rafael.oliveira@profusion.mobi>
2023-10-18 11:59:37 -0300

    feat(object-storage): add worker configuration

--- mgc/sdk/static/object_storage/s3/config.go
+++ mgc/sdk/static/object_storage/s3/config.go
@@ -6,6 +6,7 @@ type Config struct {
 	AccessKeyID string `json:"accessKeyId" jsonschema:"description=Access Key ID for S3 Credentials"`
 	SecretKey   string `json:"secretKey" jsonschema:"description=Secret Key for S3 Credentials"`
 	Token       string `json:"token,omitempty" jsonschema:"description=Token for S3 Credentials"`
+	Workers     int    `json:"workers,omitempty" jsonschema:"description=Number of routines that spawn to do parallel operations within object_storage,default=5,exlusiveMinimum=0"`
 	Region      string `json:"region,omitempty" jsonschema:"description=Region to reach the service,default=br-ne-1,enum=br-ne-1,enum=br-ne-2,enum=br-se-1"`
 	// See more about the 'squash' directive here: https://pkg.go.dev/github.com/mitchellh/mapstructure#hdr-Embedded_Structs_and_Squashing
 	config.NetworkConfig `json:",squash"` // nolint
