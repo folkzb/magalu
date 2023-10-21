7c6e72b850cddb8e51e8f41560216c871d523515
Gustavo Sverzut Barbieri <barbieri@profusion.mobi>
2023-10-21 12:52:56 -0300

    feat(core): add SimpleExecutor
    It implements the basics of executor

---
+++ mgc/core/simple_executor.go
@@ -0,0 +1,111 @@
+package core
+
+import (
+	"context"
+	"errors"
+	"fmt"
+)
+
+type LinksSpecFn func() map[string]Linker
+type RelatedSpecFn func() map[string]Executor
+type ExecutorSpecFn func(executor Executor, context context.Context, parameters Parameters, configs Configs) (result Result, err error)
+
+type ExecutorSpec struct {
+	DescriptorSpec
+	ParametersSchema *Schema
+	ConfigsSchema    *Schema
+	ResultSchema     *Schema
+	Links            LinksSpecFn
+	Related          RelatedSpecFn
+	Execute          ExecutorSpecFn
+}
+
+var errNil = errors.New("cannot be nil")
+
+func (s *ExecutorSpec) Validate() (err error) {
+	err = s.DescriptorSpec.Validate()
+	if err != nil {
+		return
+	}
+
+	if s.ParametersSchema == nil {
+		return &ChainedError{"ParametersSchema", errNil}
+	} else if s.ParametersSchema.Type != "object" {
+		return &ChainedError{"ParametersSchema", fmt.Errorf("want object, got %q", s.ParametersSchema.Type)}
+	}
+
+	if s.ConfigsSchema == nil {
+		return &ChainedError{"ConfigsSchema", errNil}
+	} else if s.ConfigsSchema.Type != "object" {
+		return &ChainedError{"ConfigsSchema", fmt.Errorf("want object, got %q", s.ConfigsSchema.Type)}
+	}
+
+	if s.ResultSchema == nil {
+		return &ChainedError{"ResultSchema", errNil}
+	}
+
+	if s.Execute == nil {
+		return &ChainedError{"Execute", errNil}
+	}
+
+	return nil
+}
+
+type SimpleExecutor struct {
+	SimpleDescriptor
+
+	parametersSchema *Schema
+	configsSchema    *Schema
+	resultSchema     *Schema
+	links            LinksSpecFn
+	related          RelatedSpecFn
+	execute          ExecutorSpecFn
+}
+
+func NewSimpleExecutor(spec ExecutorSpec) *SimpleExecutor {
+	err := spec.Validate()
+	if err != nil {
+		logger().Fatalw("invalid spec", "spec", spec, "err", err)
+	}
+	return &SimpleExecutor{
+		SimpleDescriptor{spec.DescriptorSpec},
+		spec.ParametersSchema,
+		spec.ConfigsSchema,
+		spec.ResultSchema,
+		spec.Links,
+		spec.Related,
+		spec.Execute,
+	}
+}
+
+func (e *SimpleExecutor) ParametersSchema() *Schema {
+	return e.parametersSchema
+}
+
+func (e *SimpleExecutor) ConfigsSchema() *Schema {
+	return e.configsSchema
+}
+
+func (e *SimpleExecutor) ResultSchema() *Schema {
+	return e.resultSchema
+}
+
+func (e *SimpleExecutor) Links() map[string]Linker {
+	if e.links == nil {
+		return nil
+	}
+	return e.links()
+}
+
+func (e *SimpleExecutor) Related() map[string]Executor {
+	if e.related == nil {
+		return nil
+	}
+	return e.related()
+}
+
+func (e *SimpleExecutor) Execute(context context.Context, parameters Parameters, configs Configs) (result Result, err error) {
+	return e.execute(e, context, parameters, configs)
+}
+
+var _ Executor = (*SimpleExecutor)(nil)
