78ddf3bfaf17302d32a1881884e32354ed57e552
Rafael Oliveira <rafael.oliveira@profusion.mobi>
2023-11-01 16:11:15 -0300

    fix(pipeline): fix consumer error reporting

--- mgc/core/pipeline/consumers.go
+++ mgc/core/pipeline/consumers.go
@@ -4,13 +4,8 @@ import "context"
 
 func SliceItemConsumer[S ~[]T, T any](ctx context.Context, inputChan <-chan T) (result S, err error) {
 	for input := range inputChan {
-		select {
-		case <-ctx.Done():
-			return result, context.Cause(ctx)
-		default:
-			result = append(result, input)
-		}
+		result = append(result, input)
 	}
 
-	return result, nil
+	return result, context.Cause(ctx)
 }
