d4e2c5469889da34ae8fc5fc36e1d4a84c6b3a68
Stéfano Chiavegatto <stefano.lopes@profusion.mobi>
2023-11-08 15:56:06 -0300

    feat: add version of SliceItemConsumer() with item limit
    Part of #452

--- mgc/core/pipeline/consumers.go
+++ mgc/core/pipeline/consumers.go
@@ -14,3 +14,20 @@ func SliceItemConsumer[S ~[]T, T any](ctx context.Context, inputChan <-chan T) (
 
 	return result, context.Cause(ctx)
 }
+
+func SliceItemLimitedConsumer[S ~[]T, T any](ctx context.Context, limit int, inputChan <-chan T) (result S, err error) {
+	for input := range inputChan {
+		select {
+		case <-ctx.Done():
+			return result, context.Cause(ctx)
+		default:
+			result = append(result, input)
+
+			if limit > 0 && len(result) >= limit {
+				return result, context.Cause(ctx)
+			}
+		}
+	}
+
+	return result, context.Cause(ctx)
+}
