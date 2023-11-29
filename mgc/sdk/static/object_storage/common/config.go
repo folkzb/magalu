cbe4d860aeba2e95771902597fa2ff7e8cd49b7c
Pedro Mota <pedro.mota@profusion.mobi>
2023-11-29 11:56:21 -0300

    fix(object-storage): remove unused Token property
    Issue #561

--- mgc/sdk/static/object_storage/common/config.go
+++ mgc/sdk/static/object_storage/common/config.go
@@ -3,7 +3,6 @@ package common
 import "magalu.cloud/core/config"
 
 type Config struct {
-	Token   string `json:"token,omitempty" jsonschema:"description=Token for object-storage Credentials"`
 	Workers int    `json:"workers,omitempty" jsonschema:"description=Number of routines that spawn to do parallel operations within object_storage,default=5,exlusiveMinimum=0"`
 	Region  string `json:"region,omitempty" jsonschema:"description=Region to reach the service,default=br-ne-1,enum=br-ne-1,enum=br-ne-2,enum=br-se-1"`
 	// See more about the 'squash' directive here: https://pkg.go.dev/github.com/mitchellh/mapstructure#hdr-Embedded_Structs_and_Squashing
