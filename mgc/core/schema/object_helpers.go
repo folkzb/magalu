34b94f71cdb610c9baf4e000d4cae6f54fe8a04c
Leonardo Bronstein Franceschetti <leonardo.franceschetti@profusion.mobi>
2023-12-11 13:08:31 -0500

    feat(schema): add standard way to loop over xOf sub-schemas

--- mgc/core/schema/object_helpers.go
+++ mgc/core/schema/object_helpers.go
@@ -38,7 +38,11 @@ func CollectAllObjectPropertySchemas(s *Schema) (m map[string][]ObjectPropertySc
 
 type ForEachObjectPropertyCb func(ps ObjectPropertySchema) (
 	run bool, // if true, keep running. False early aborts the walk
-	err error, // if any error should be reported
+	err error, // if not nill, aborts the walk and returns in the function
+)
+type ForEachXOfCb func(ps XOfChildSchema) (
+	run bool, // if true, keep running. False early aborts the walk
+	err error, // if not nill, aborts the walk and returns in the function
 )
 
 // JSON Schema allows properties to live in both the root level or at AllOf, AnyOf, OneOf. This visits them all.
@@ -50,6 +54,35 @@ type ForEachObjectPropertyCb func(ps ObjectPropertySchema) (
 // Finished is true if all entries were processed. It's false if it was early aborted
 func ForEachObjectProperty(s *Schema, cb ForEachObjectPropertyCb) (finished bool, err error) {
 	return forEachObjectProperty(ObjectPropertySchema{Container: s}, cb)
+}
+
+type XOfChildSchema struct {
+	Field  string
+	Index  int
+	Schema *Schema
+}
+
+func ForEachXOf(s *Schema, cb ForEachXOfCb) (finished bool, err error) {
+	finished = true
+	for i, anyOf := range s.AnyOf {
+		run, err := cb(XOfChildSchema{"AnyOf", i, (*Schema)(anyOf.Value)})
+		if !run || err != nil {
+			return false, err
+		}
+	}
+	for i, oneOf := range s.OneOf {
+		run, err := cb(XOfChildSchema{"OneOf", i, (*Schema)(oneOf.Value)})
+		if !run || err != nil {
+			return false, err
+		}
+	}
+	for i, allOf := range s.AllOf {
+		run, err := cb(XOfChildSchema{"AllOf", i, (*Schema)(allOf.Value)})
+		if !run || err != nil {
+			return false, err
+		}
+	}
+	return
 }
 
 func forEachObjectProperty(ps ObjectPropertySchema, cb ForEachObjectPropertyCb) (finished bool, err error) {
