65ab30acb91b5234bf14bcee009e43d6873f2aed
Pedro Mota <pedro.mota@profusion.mobi>
2023-12-14 17:06:26 -0300

    feat(object-storage/objects/acl): add object acl set operation

---
+++ mgc/sdk/static/object_storage/objects/acl/set.go
@@ -0,0 +1,78 @@
+package acl
+
+import (
+	"context"
+	"fmt"
+
+	"net/http"
+
+	"magalu.cloud/core"
+	mgcSchemaPkg "magalu.cloud/core/schema"
+	"magalu.cloud/core/utils"
+	"magalu.cloud/sdk/static/object_storage/common"
+)
+
+type setObjectACLParams struct {
+	Destination            mgcSchemaPkg.URI `json:"dst" jsonschema:"description=The full object URL to set the ACL information,example:my-bucket/file.txt" mgc:"positional"`
+	AwsExecRead            bool             `json:"aws_exec_read,omitempty"`
+	BucketOwnerRead        bool             `json:"bucket_owner_read,omitempty"`
+	BucketOwnerFullControl bool             `json:"bucket_owner_full_control,omitempty"`
+	common.ACLPermissions  `json:",squash"` // nolint
+}
+
+var getSet = utils.NewLazyLoader(func() core.Executor {
+	return core.NewStaticExecute(
+		core.DescriptorSpec{
+			Name:        "set",
+			Description: "Set ACL information for the specified object",
+		},
+		set,
+	)
+})
+
+func set(ctx context.Context, p setObjectACLParams, cfg common.Config) (result common.AccessControlPolicy, err error) {
+	err = p.ACLPermissions.Validate()
+	if err != nil {
+		return
+	}
+
+	req, err := newSetObjectAclRequest(ctx, p, cfg)
+	if err != nil {
+		return
+	}
+
+	resp, err := common.SendRequest(ctx, req)
+	if err != nil {
+		return
+	}
+
+	return common.UnwrapResponse[common.AccessControlPolicy](resp, req)
+}
+
+func newSetObjectAclRequest(ctx context.Context, p setObjectACLParams, cfg common.Config) (*http.Request, error) {
+	url, err := common.BuildBucketHostWithPathURL(cfg, common.NewBucketNameFromURI(p.Destination), p.Destination.Path())
+	if err != nil {
+		return nil, core.UsageError{Err: err}
+	}
+
+	query := url.Query()
+	query.Add("acl", "")
+
+	url.RawQuery = query.Encode()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPut, url.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if p.ACLPermissions.IsEmpty() {
+		return nil, core.UsageError{Err: fmt.Errorf("needs to pass either grant permissions or canned info")}
+	}
+
+	err = p.ACLPermissions.SetHeaders(req, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
