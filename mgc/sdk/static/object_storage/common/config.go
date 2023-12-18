4ebe396842a4425b096a387177676a0aaf84269e
Stéfano Chiavegatto <stefano.lopes@profusion.mobi>
2023-12-18 15:11:54 -0300

    fix(cli): remove unused "br-ne-2" region
    The "br-ne-2" region is not used and so all instances of it in the code
    have been removed.

--- mgc/sdk/static/object_storage/common/config.go
+++ mgc/sdk/static/object_storage/common/config.go
@@ -4,7 +4,7 @@ import "magalu.cloud/core/config"
 
 type Config struct {
 	Workers int    `json:"workers,omitempty" jsonschema:"description=Number of routines that spawn to do parallel operations within object_storage,default=5,minimum=1"`
-	Region  string `json:"region,omitempty" jsonschema:"description=Region to reach the service,default=br-ne-1,enum=br-ne-1,enum=br-ne-2,enum=br-se-1"`
+	Region  string `json:"region,omitempty" jsonschema:"description=Region to reach the service,default=br-ne-1,enum=br-ne-1,enum=br-se-1"`
 	// See more about the 'squash' directive here: https://pkg.go.dev/github.com/mitchellh/mapstructure#hdr-Embedded_Structs_and_Squashing
 	config.NetworkConfig `json:",squash"` // nolint
 }
