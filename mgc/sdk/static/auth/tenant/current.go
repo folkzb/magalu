13c04e5dba4b6ddaeefd58eb4fb9cf3c20ff59c5
Leonardo Bronstein Franceschetti <leonardo.franceschetti@profusion.mobi>
2024-01-04 18:33:18 -0500

    feat(auth): fetch full Tenant object on CurrentTenant executor

--- mgc/sdk/static/auth/tenant/current.go
+++ mgc/sdk/static/auth/tenant/current.go
@@ -5,39 +5,25 @@ import (
 	"fmt"
 
 	"magalu.cloud/core"
+	"magalu.cloud/core/auth"
 	mgcAuthPkg "magalu.cloud/core/auth"
 	"magalu.cloud/core/utils"
 )
 
-type tenantCurrentResult struct {
-	ID string
-}
-
-var getCurrent = utils.NewLazyLoader[core.Executor](newCurrent)
-
-func newCurrent() core.Executor {
+var getCurrent = utils.NewLazyLoader[core.Executor](func() core.Executor {
 	return core.NewStaticExecuteSimple(
 		core.DescriptorSpec{
 			Name:        "current",
 			Summary:     "Get the currently active Tenant",
 			Description: `The current Tenant is used for all Magalu HTTP requests`,
 		},
-		func(ctx context.Context) (*tenantCurrentResult, error) {
+		func(ctx context.Context) (*auth.Tenant, error) {
 			auth := mgcAuthPkg.FromContext(ctx)
 			if auth == nil {
 				return nil, fmt.Errorf("unable to get auth from context")
 			}
 
-			id, err := auth.CurrentTenantID()
-			if err != nil {
-				return nil, err
-			}
-
-			if id == "" {
-				return nil, fmt.Errorf("current tenant ID is empty. Try logging in at least once or run the 'auth tenant select' operation")
-			}
-
-			return &tenantCurrentResult{ID: id}, nil
+			return auth.CurrentTenant(ctx)
 		},
 	)
-}
+})
