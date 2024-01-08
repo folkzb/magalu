12980388dfbb33660e1bb426f2f5be6a9ef02e02
Leonardo Bronstein Franceschetti <leonardo.franceschetti@profusion.mobi>
2024-01-08 16:26:35 -0500

    chore(tf): format example main.tf file

--- mgc/terraform-provider-mgc/examples/provider/main.tf
+++ mgc/terraform-provider-mgc/examples/provider/main.tf
@@ -1,10 +1,10 @@
 terraform {
-    required_providers {
-        mgc = {
-            version = "0.1"
-            source = "magalucloud/mgc"
-        }
+  required_providers {
+    mgc = {
+      version = "0.1"
+      source  = "magalucloud/mgc"
     }
+  }
 }
 
 provider "mgc" {}
@@ -17,18 +17,18 @@ resource "mgc_virtual-machine_instances" "myvm" {
   image = {
     name = "cloud-ubuntu-22.04 LTS"
   }
-  ssh_key_name = "luizalabs-key"
+  ssh_key_name      = "luizalabs-key"
   availability_zone = "br-ne-1c"
 }
 
 resource "mgc_block-storage_volume" "myvmvolume" {
-    name = "myvmvolume"
-    description = "myvmvolumedescription"
-    size = 20
-    volume_type = "cloud_nvme"
+  name        = "myvmvolume"
+  description = "myvmvolumedescription"
+  size        = 20
+  volume_type = "cloud_nvme"
 }
 
 resource "mgc_block-storage_volume_attach" "myvmvolumeattachment" {
-    id = mgc_block-storage_volume.myvmvolume.id
-    virtual_machine_id = mgc_virtual-machine_instances.myvm.id
+  id                 = mgc_block-storage_volume.myvmvolume.id
+  virtual_machine_id = mgc_virtual-machine_instances.myvm.id
 }
