ddc6b25c7c86a25d6a7758475deb768888a38f82
Stéfano Chiavegatto <stefano.lopes@profusion.mobi>
2024-01-25 19:08:52 -0300

    feat(progress-report): add functions to report progress for write

---
+++ mgc/core/progress_report/reporter_writer.go
@@ -0,0 +1,38 @@
+package progress_report
+
+import (
+	"io"
+)
+
+type ReportWrite func(n int, err error)
+
+type reporterWriter struct {
+	parent         io.Writer
+	reportProgress ReportWrite
+}
+
+func NewReporterWriter(parent io.Writer, reportProgress ReportWrite) *reporterWriter {
+	return &reporterWriter{
+		parent:         parent,
+		reportProgress: reportProgress,
+	}
+}
+
+func (rw *reporterWriter) Unwrap() io.Writer {
+	return rw.parent
+}
+
+func (rw *reporterWriter) Write(p []byte) (n int, err error) {
+	n, err = rw.parent.Write(p)
+	rw.reportProgress(n, err)
+	return
+}
+
+func (rw *reporterWriter) Close() error {
+	if closer, ok := rw.parent.(io.Closer); ok {
+		return closer.Close()
+	}
+	return nil
+}
+
+var _ io.WriteCloser = (*reporterWriter)(nil)
