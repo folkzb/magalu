13c8a948033fd827f86dfab5b9adec38fed475c2
Gefferson Ferraz <me@gefferson.com.br>
2024-04-09 16:11:12 -0300

    fix: issue 850- presign wo key (#968)

--- mgc/sdk/static/object_storage/objects/presign.go
+++ mgc/sdk/static/object_storage/objects/presign.go
@@ -64,6 +64,20 @@ func presign(ctx context.Context, p presignObjectParams, cfg common.Config) (pre
 }
 
 func newPresignedRequest(ctx context.Context, cfg common.Config, p presignObjectParams) (*http.Request, error) {
+	headFile, err := common.HeadFile(ctx, cfg, p.Destination, "")
+	if err != nil {
+		return nil, err
+	}
+
+	if len(headFile.ETag) == 0 {
+		return nil, core.UsageError{Err: fmt.Errorf("at least one key is required. Your input: %s", p.Destination)}
+	}
+
+	// path := p.Destination.Path()
+	// if !strings.Contains(path, "/") {
+	// 	return nil, core.UsageError{Err: fmt.Errorf("at least one key is required. Your input: %s", p.Destination)}
+	// }
+
 	host, err := common.BuildBucketHostWithPath(cfg, common.NewBucketNameFromURI(p.Destination), p.Destination.Path())
 	if err != nil {
 		return nil, core.UsageError{Err: err}
