fd7aacfbcc844979fb8a8b1a78cb9784776bd7f5
Gefferson Ferraz <me@gefferson.com.br>
2024-06-08 11:43:02 -0300

    feat: add panic recover (#1061)

--- mgc/cli/main.go
+++ mgc/cli/main.go
@@ -3,14 +3,32 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
+	"strings"
 
 	"magalu.cloud/cli/cmd"
+	mgcSdk "magalu.cloud/sdk"
 )
 
 func main() {
+	defer panicRecover()
+
 	err := cmd.Execute()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func panicRecover() {
+	err := recover()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "\n😔 Oops! Something went wrong.\nPlease help us improve by sending the error report to: https://help.magalu.cloud/hc/pt-br/requests/new\n\n  Version: %s\n  SO: %s / %s\n  Args: %s\n  Error: %s\n\nThank you for your cooperation!\n\n",
+			mgcSdk.Version,
+			runtime.GOOS,
+			runtime.GOARCH,
+			strings.Join(os.Args, " "),
+			err)
 		os.Exit(1)
 	}
 }
