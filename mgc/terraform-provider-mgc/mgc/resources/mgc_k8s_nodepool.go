9818f270c1ea062ccb466c9c55564551fbdef7ef
Felipe Canton Públio <felipe@publio.dev>
2024-11-04 17:48:03 -0300

    [tf] fix: Fix nodepools import error (#1236)
    [tf] fix: nodepools import error

--- mgc/terraform-provider-mgc/mgc/resources/mgc_k8s_nodepool.go
+++ mgc/terraform-provider-mgc/mgc/resources/mgc_k8s_nodepool.go
@@ -285,6 +285,7 @@ func (r *NewNodePoolResource) ImportState(ctx context.Context, req resource.Impo
 		return
 	}
 
+	data.ClusterID = types.StringValue(ids[0])
 	data.NodePool = tfutil.ConvertToNodePoolGet(&nodepool)
 	resp.Diagnostics.Append(resp.State.Set(ctx, data)...)
 }
