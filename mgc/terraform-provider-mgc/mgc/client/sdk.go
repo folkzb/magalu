16c8811f625d5f51d68415f7973bd755e4bc79da
Gefferson Ferraz <me@gefferson.com.br>
2024-11-06 11:01:13 -0300

    fix: tf - remove required api key (#1240)
    * fix: tf - remove required api key

--- mgc/terraform-provider-mgc/mgc/client/sdk.go
+++ mgc/terraform-provider-mgc/mgc/client/sdk.go
@@ -46,11 +46,9 @@ func NewSDKClient[T SDKFrom](req T) (*mgcSdk.Client, error, error) {
 		_ = sdkClient.Sdk().Config().SetTempConfig("env", config.Env.ValueString())
 	}
 
-	if config.ApiKey.ValueString() == "" {
-		return nil, fmt.Errorf("provider with api_key must be setted"), fmt.Errorf(`please check the resource to see if they are using 'provider' and verify if the provider has the 'api_key' correctly set`)
+	if config.ApiKey.ValueString() != "" {
+		_ = sdkClient.Sdk().Auth().SetAPIKey(config.ApiKey.ValueString())
 	}
-
-	_ = sdkClient.Sdk().Auth().SetAPIKey(config.ApiKey.ValueString())
 
 	if config.ObjectStorage != nil && config.ObjectStorage.ObjectKeyPair != nil {
 		sdkClient.Sdk().Config().AddTempKeyPair("apikey", config.ObjectStorage.ObjectKeyPair.KeyID.ValueString(),
