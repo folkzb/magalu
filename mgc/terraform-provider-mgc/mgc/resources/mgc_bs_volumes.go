3deef50b747ae8eb59326043e90724a4218a878c
Felipe Canton Públio <felipe@publio.dev>
2025-01-20 15:55:34 -0300

    fix: handling of availability zones for block storage volumes (#1331)
    * fix: block storage volumes with availabily zones sending empty value
    
    * chore - fix indentation

--- mgc/terraform-provider-mgc/mgc/resources/mgc_bs_volumes.go
+++ mgc/terraform-provider-mgc/mgc/resources/mgc_bs_volumes.go
@@ -134,7 +134,6 @@ func (r *bsVolumes) Schema(_ context.Context, _ resource.SchemaRequest, resp *re
 				Computed:    true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplace(),
-					stringplanmodifier.UseStateForUnknown(),
 				},
 			},
 			"size": schema.Int64Attribute{
@@ -190,10 +189,9 @@ func (r *bsVolumes) Create(ctx context.Context, req resource.CreateRequest, resp
 	}
 
 	createParam := sdkBlockStorageVolumes.CreateParameters{
-		Name:             state.Name.ValueString(),
-		Size:             int(state.Size.ValueInt64()),
-		Encrypted:        state.Encrypted.ValueBoolPointer(),
-		AvailabilityZone: state.AvailabilityZone.ValueStringPointer(),
+		Name:      state.Name.ValueString(),
+		Size:      int(state.Size.ValueInt64()),
+		Encrypted: state.Encrypted.ValueBoolPointer(),
 		Type: sdkBlockStorageVolumes.CreateParametersType{
 			Name: state.Type.ValueStringPointer(),
 		},
@@ -202,6 +200,9 @@ func (r *bsVolumes) Create(ctx context.Context, req resource.CreateRequest, resp
 		createParam.Snapshot = &sdkBlockStorageVolumes.CreateParametersSnapshot{
 			Id: state.SnapshotID.ValueStringPointer(),
 		}
+	}
+	if !state.AvailabilityZone.IsUnknown() {
+		createParam.AvailabilityZone = state.AvailabilityZone.ValueStringPointer()
 	}
 
 	createResult, err := r.bsVolumes.CreateContext(ctx, createParam,
